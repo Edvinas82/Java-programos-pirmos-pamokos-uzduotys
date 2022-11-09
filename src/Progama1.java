d1ef68cac4c574c636e9010c576866bb9d6c8b22
0mindaugas0 <zaniuk4@hotmail.com>
2022-11-09 10:34:21 +0200

    Sukurtas projektas

---
+++ src/Progama1.java
@@ -0,0 +1,18 @@
+public class Progama1 {
+    public static void main(String[] args) {
+        int a=39;
+        short b=5896;
+        double  c=6.13235;
+
+        long d=a;
+        int e= b;
+        byte f= (byte)b;
+
+        char x='a';
+        char y='b';
+        char z='c';
+        String suma = ""+x+y+z;
+        System.out.println(suma);
+    }
+
+}
